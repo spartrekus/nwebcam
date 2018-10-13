34f2d017b84204b4899ed08865f54668fff02f1e
spartrekus <spartrekus@gmail.com>
2018-10-13 08:47:32 +0100

    Add files via upload

---
+++ nwebcam.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    system( " mplayer  -tv device=/dev/video0 tv://  ");
+    return 0;
+}
+
+
